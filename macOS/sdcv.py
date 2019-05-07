f3fa38a241681883e7f1c334ccab805807d2bfd4
eph <eph@EphMacBook.local>
2019-05-08 00:39:59 +0800

    添加sdcv workflow工具

---
+++ macOS/sdcv.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+'''alfred sdcv workflow 包装器'''
+
+import sys
+import subprocess
+import json
+
+def main(argv):
+    word = argv[1]
+    p = subprocess.Popen(['/usr/local/bin/sdcv', '-nj', word],
+                         stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    out, err = p.communicate()
+
+    d = json.loads(out)
+
+    result = {'items': []}
+
+    for one in d:
+        item = {
+            #"uid": "desktop",
+            #"type": "file",
+            "title": one['word'],
+            "subtitle": one['definition'].replace('\n', ' '),
+            "arg": word,
+            #"autocomplete": "Desktop",
+            "icon": {
+                "type": "dictionary",
+            }
+        }
+        result['items'].append(item)
+
+    print(json.dumps(result, indent=2, ensure_ascii=False))
+
+if __name__ == '__main__':
+    main(sys.argv)
